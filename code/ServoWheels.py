c2f5b133fe7c59cf809884581a79ad596a09ea83
Eric Rohlfs <ericghent@gmail.com>
2017-04-10 17:10:02 -0400

    Update ServoWheels.py
    Adding ability to reverse the left and right function needed when different robots have servos mounted differently.

--- code/ServoWheels.py
+++ code/ServoWheels.py
@@ -27,15 +27,23 @@ class ServoWheels:
           self.basics = ServoBasics(self._pwm)
           self._left_wheel = left_wheel
           self._right_wheel = right_wheel
+          self._reverse_left_right = false
     
     #for the turn left and turn right functions, edit the sleep values for back up and turn to get the servo timing correct
     def turn_left(self):
-          self.basics.servo_clockwise(self._left_wheel)
-          self.basics.servo_clockwise(self._right_wheel)
+          if(self._reverse_left_right):
+               self.turn_right()
+          else:
+               self.basics.servo_clockwise(self._left_wheel)
+               self.basics.servo_clockwise(self._right_wheel)
 
     def turn_right(self):
-          self.basics.servo_counter_clockwise(self._left_wheel)
-          self.basics.servo_counter_clockwise(self._right_wheel)
+          if(self._reverse_left_right):
+               self.turn_left()
+          else:
+               self.basics.servo_counter_clockwise(self._left_wheel)
+               self.basics.servo_counter_clockwise(self._right_wheel)
+            
     def foward(self):
           self.basics.servo_clockwise(self._left_wheel)
           self.basics.servo_counter_clockwise(self._right_wheel)
@@ -43,5 +51,12 @@ class ServoWheels:
     def backward(self):
           self.basics.servo_counter_clockwise(self._left_wheel)
           self.basics.servo_clockwise(self._right_wheel)
+        
+    @property
+    def reverse_left_right(self):
+        return self._reverse_left_right
+    @reverse_left_right.setter
+    def reverse_left_right(self,value):
+        self._reverse_left_right = value
           
           
